580d062845053ff4cb13b7483a7d55fee108f864
Jesiel Viana <jesielviana@proton.me>
2024-08-01 16:06:56 -0300

    criando novo banco

--- prisma/migrations/20240801190643_init/migration.sql
+++ prisma/migrations/20240801190643_init/migration.sql
@@ -1,12 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `movie` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE "movie";
-
 -- CreateTable
 CREATE TABLE "movies" (
     "id" SERIAL NOT NULL,
