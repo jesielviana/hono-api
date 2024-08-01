9010a9d4775363ff6e94d1cad58fd6b993aa2fd5
Jesiel Viana <jesielviana@proton.me>
2024-08-01 15:51:22 -0300

    add autenticação com jwt

---
+++ prisma/migrations/20240801145006_users/migration.sql
@@ -0,0 +1,32 @@
+/*
+  Warnings:
+
+  - You are about to drop the `movie` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "movie";
+
+-- CreateTable
+CREATE TABLE "movies" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "releaseYear" INTEGER NOT NULL,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "movies_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,26 @@
-import { Hono } from "hono";
-import { logger } from "hono/logger";
-import moviesRoute from "./routes/movies";
-import { cors } from "hono/cors";
-import { csrf } from "hono/csrf";
-import { secureHeaders } from "hono/secure-headers";
 import { swaggerUI } from "@hono/swagger-ui";
+import { Hono } from "hono";
+import { jwt } from "hono/jwt";
 import { serveStatic } from "hono/bun";
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
+import { secureHeaders } from "hono/secure-headers";
 import swagger from "../swagger.json";
+import moviesRoute from "./routes/movies";
+import usersRoute from "./routes/users";
+import loginRoute from "./routes/login";
 
 const api = new Hono().basePath("/api");
 api.use(cors());
 api.use(logger());
-// api.use(csrf());
 api.use(secureHeaders());
+api.use("/auth/*", jwt({ secret: Bun.env.JWT_SECRET as string }));
+
+api.get("/auth/page", (c) => {
+  console.log("You are authorized");
+  const payload = c.get("jwtPayload");
+  return c.json(payload);
+});
 
 api.get("/", (c) => {
   return c.text("Api Online!");
@@ -33,9 +41,10 @@ api.onError((err, c) => {
 });
 
 api.route("/movies", moviesRoute);
+api.route("/users", usersRoute);
+api.route("/login", loginRoute);
 
 const home = new Hono();
-// home.get("/", (c) => c.html(index));
 home.get("/", serveStatic({ path: "./index.html" }));
 home.get("/favicon.ico", serveStatic({ path: "./favicon.ico" }));
 
