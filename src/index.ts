4eeb4be96fbf553d5fec88d9867dd02a592d015d
Jesiel Viana <jesielviana@proton.me>
2024-08-08 17:38:09 -0300

    removendo página inicial

--- src/index.ts
+++ src/index.ts
@@ -10,47 +10,35 @@ import moviesRoute from "./routes/movies";
 import usersRoute from "./routes/users";
 import loginRoute from "./routes/login";
 
-const api = new Hono().basePath("/api");
-api.use(cors());
-api.use(logger());
-api.use(secureHeaders());
-api.use("/auth/*", jwt({ secret: Bun.env.JWT_SECRET as string }));
+const app = new Hono();
+app.use(cors());
+app.use(logger());
+app.use(secureHeaders());
+app.use("/auth/*", jwt({ secret: Bun.env.JWT_SECRET as string }));
 
-api.get("/auth/page", (c) => {
+app.get("/auth/page", (c) => {
   console.log("You are authorized");
   const payload = c.get("jwtPayload");
   return c.json(payload);
 });
 
-api.get("/", (c) => {
-  return c.text("Api Online!");
-});
-
-api.get("/doc", swaggerUI({ url: "/api/swagger" }));
-api.get("/swagger", (c) => {
+app.get("/", swaggerUI({ url: "/doc" }));
+app.get("/doc", (c) => {
   return c.json(swagger);
 });
 
-api.notFound((c) => {
+app.notFound((c) => {
   return c.text("Rota inválida", 404);
 });
 
-api.onError((err, c) => {
+app.onError((err, c) => {
   console.error(`${err}`);
   return c.text("Erro interno", 500);
 });
 
-api.route("/movies", moviesRoute);
-api.route("/users", usersRoute);
-api.route("/login", loginRoute);
-
-const home = new Hono();
-home.get("/", serveStatic({ path: "./index.html" }));
-home.get("/favicon.ico", serveStatic({ path: "./favicon.ico" }));
-
-const app = new Hono();
-app.route("/", home);
-app.route("/", api);
+app.route("/api/movies", moviesRoute);
+app.route("/api/users", usersRoute);
+app.route("/api/login", loginRoute);
 
 export default {
   port: process.env.PORT || 3000,
